57e5d8b0f890338e7f616d551c9eba50b28bea6c
Mark Liu <mark@biro.ai>
2021-02-15 13:39:41 -0500

    Remove stray command in script

--- save_model.py
+++ save_model.py
@@ -2,5 +2,3 @@ from transformers import DistilBertForTokenClassification
 
 model = DistilBertForTokenClassification.from_pretrained('distilbert-base-uncased', num_labels=3)
 model.save_pretrained("model")
-
-python -m transformers.convert_graph_to_onnx --model ../model --framework pt --tokenizer distilbert-base-uncased out.onnx
